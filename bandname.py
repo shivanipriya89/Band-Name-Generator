c7efa2fbcb3f48686a19c17d04038fb266dd7c9f
shivanipriya89 <83451849+shivanipriya89@users.noreply.github.com>
2022-08-18 22:06:28 +0530

    Add files via upload
    This is a simple Project of Band Name Generator, which takes the username and petname from the user and then
    publish it on the shell

---
+++ bandname.py
@@ -0,0 +1,10 @@
+#Band Name Generator
+
+print("Welcome to the Band Name Generator!")
+usercity=str(input('''
+What's name of the city you grew up in?
+'''))
+petname=str(input('''
+What's your pet name?
+'''))
+print("Your Band name could be",usercity,petname)
